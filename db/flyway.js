df9dfe2b678bb39dd87309187c743537c9b61268
Christian Lenz <christian.lenz@de.ibm.com>
2021-05-06 14:51:38 +0200

    adjusted documentation for flyway config

--- db/flyway.js
+++ db/flyway.js
@@ -2,14 +2,17 @@
 module.exports = function() {
     return {
         flywayArgs: {
+            // JDBC string that addresses the database
             url: 'jdbc:postgresql://localhost:15432/compass',
+            // the default schema of a Postgresql database
             schemas: 'public',
+            // local location where the db migration scripts are stored
             locations: 'filesystem:db/migration',
 
             // if not set here, flyway will ask during execution
             // user: 'DB_USER',
             // password: 'DB_PASSWORD',
         },
-        // version: '7.5.0', // optional, empty or missing will download the latest
+        // version: '7.8.2', // optional, empty or missing will download the latest (7.8.2 is known to be working)
     };
 };
