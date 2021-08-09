67fc3bcd240c5b6519750aedccdd34e8933d6b04
Alena Schmickl <alena.schmickl@de.ibm.com>
2021-08-09 17:04:11 +0200

    Beautified code

--- src/controllers/SubjectIdentitiesController.ts
+++ src/controllers/SubjectIdentitiesController.ts
@@ -29,34 +29,40 @@ export class SubjectIdentitiesController {
      * Add new subject.
      * Is called by API user to register a new subject which will then get access to app by their id.
      */
-     @Post('addNew')
-     @Middleware([AuthorizationController.checkApiUserLogin])
-     public async addSubjectIdentity(req: ISecureRequest, resp: Response) {
-         try {
-             // validate parameter existance
-             if (!req.body.subjectIdentity || !req.body.subjectIdentity.recordId) {
-                 return resp.status(400).send({ error: 'missing_data' });
-             }
-            
+    @Post('addNew')
+    @Middleware([AuthorizationController.checkApiUserLogin])
+    public async addSubjectIdentity(req: ISecureRequest, resp: Response) {
+        try {
+            // validate parameter existance
+            if (!req.body.subjectIdentity || !req.body.subjectIdentity.recordId) {
+                return resp.status(400).send({
+                    error: 'missing_data'
+                });
+            }
+
             try {
                 var subjectIdentityExistence: boolean = await this.subjectIdentityModel.getSubjectIdentityExistence(
                     req.body.subjectIdentity.recordId
                 );
-            } catch {//no action required as participant doesn't exist
-            }
-            
-             if (subjectIdentityExistence) {
-                return resp.status(409).send({ error: 'participant_already_exists' });
+            } catch { //no action required as participant doesn't exist
             }
 
-             await this.subjectIdentityModel.addNewSubjectIdentity(
-                 req.body.subjectIdentity.recordId
-             );
- 
-             return resp.status(200).json({ return: true });
-         } catch (err) {
-             Logger.Err(err, true);
-             return resp.sendStatus(500);
-         }
-     }
+            if (subjectIdentityExistence) {
+                return resp.status(409).send({
+                    error: 'participant_already_exists'
+                });
+            }
+
+            await this.subjectIdentityModel.addNewSubjectIdentity(
+                req.body.subjectIdentity.recordId
+            );
+
+            return resp.status(200).json({
+                return: true
+            });
+        } catch (err) {
+            Logger.Err(err, true);
+            return resp.sendStatus(500);
+        }
+    }
 }
