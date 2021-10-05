7977fb02450187d4866ae3e4bb3d966b49fbd4d2
mbastian93 <m_bastian@outlook.de>
2021-10-05 11:00:17 +0200

    fix db migration
    remove duplicate data type

--- db/migration/V006__Questionnaire_Version_History.sql
+++ db/migration/V006__Questionnaire_Version_History.sql
@@ -5,7 +5,7 @@ CREATE TABLE questionnaire_version_history
     url character varying(355) NOT NULL,
     version character varying(355) NOT NULL,
     name character varying(355) NOT NULL,
-    body character json NOT NULL,
+    body json NOT NULL,
     CONSTRAINT questionnaire_version_history_pkey PRIMARY KEY (id),
     CONSTRAINT fk_name FOREIGN KEY (name) REFERENCES questionnaires(id)
 );
